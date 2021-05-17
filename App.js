3df3c4ad03aa36d4b963969f04847e140b36a273
Tope Olotu <topeolotu75@gmail.com>
2021-05-17 22:38:37 +0100

    bottom naigation bar and Touchable Layout Completed

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Home, OrderDelivery, Restaurant } from './screens';
+import Tabs from './navigation/tabs'; 
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ 
+          headerShown: false
+         }}
+
+         initialRouteName={"Home"}
+      >
+        <Stack.Screen name="Home" component={Tabs} />
+        <Stack.Screen name="Restaurant" component={Restaurant} />
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "AIzaSyB8gFl-FLJmgxjkDWO-QbjtHrtJz4tFJQE";
+
+export default GOOGLE_API_KEY;
